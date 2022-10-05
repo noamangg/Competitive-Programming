91a6f43c1f5e257263c2538cb99c0ee9092bf628
Noaman Grewaity <noamangg@gmail.com>
2022-10-05 18:57:57 +0300

    this is a commit after creating a c++ file

---
+++ 800/4A - Watermelon.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+// https://codeforces.com/problemset/problem/4/A
+using namespace std;
+
+
+// start 
+int main()
+{
+  int w;
+  cin >> w;
+
+  if(w % 2 == 0 && w != 2)
+  cout << "YES";
+
+  else cout << "NO";
+
+
+
+  return 0;
+}
+// end 123 
